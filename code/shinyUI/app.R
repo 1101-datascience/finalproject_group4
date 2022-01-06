3246e71745145fff2af41161fc63df682de69452
YJack <jackleagends@gmail.com>
2022-01-06 15:28:34 +0800

    加入 panel

--- code/shinyUI/app.R
+++ code/shinyUI/app.R
@@ -11,9 +11,54 @@ which(apply(data, 2, var)==0)
 data<-subset(data,select=-Net.Income.Flag)
 
 ui <- fluidPage(
-    plotOutput("plot"), 
-    plotOutput("plot2"), 
-    plotOutput("plot3")
+    navbarPage("NCAA Swimming",
+        tabPanel("Correlation Analysis", fluid = TRUE, icon = icon("globe-americas"),
+            sidebarLayout(
+                sidebarPanel(
+                    selectInput(inputId = "EventFinder",
+                                label = "Select Analysis",
+                                choices = c("Correlation Analysis", "PC Analysis"),
+                                selected = "50 Free",
+                                width = "220px"
+                                ),
+                ),
+                mainPanel(
+                    plotOutput("plot"), 
+                )
+            )
+        ),
+        tabPanel("PC Analysis", fluid = TRUE, icon = icon("globe-americas"),
+            sidebarLayout(
+                sidebarPanel(
+                    selectInput(inputId = "EventFinder",
+                                label = "Select Analysis",
+                                choices = c("Correlation Analysis", "PC Analysis"),
+                                selected = "50 Free",
+                                width = "220px"
+                                ),
+                ),
+                mainPanel(
+                    plotOutput("plot2"), 
+                    plotOutput("plot3"),
+                )
+            )
+        ),
+        tabPanel("Survive&Bankruptcy", fluid = TRUE, icon = icon("globe-americas"),
+            sidebarLayout(
+                sidebarPanel(
+                    selectInput(inputId = "EventFinder",
+                                label = "Select Analysis",
+                                choices = c("Correlation Analysis", "PC Analysis"),
+                                selected = "50 Free",
+                                width = "220px"
+                                ),
+                ),
+                mainPanel(
+                    plotOutput("survive_bankrupt")
+                )
+            )
+        )
+    )
 )
 
 server <- function(input, output) {
@@ -27,6 +72,7 @@ server <- function(input, output) {
         hist1 = ggplot(data, aes(Bankrupt.,)) + geom_bar(fill = c("Green","Red")) + theme_bw() + ggtitle("Survive(0) VS Bankruptcy(1)") + theme(plot.title = element_text(hjust = 0.5)) + xlab("Survive(0)                              Bankruptcy(1)")
         corr1 = ggcorr(data[,1:20],size=2,hjust=1)+geom_point()+scale_alpha_manual()+guides()
         corr2 = corrplot(corr=correlationMatrix, tl.cex=0.1)
+        # This plot shows the correlation of each features.The deeper the color the more correlation for the features.
     })
 
     output$plot2 <- renderPlot({
@@ -39,14 +85,24 @@ server <- function(input, output) {
         tot.var <- sum(data.pca.scale$sdev^2)
         var.explained <- data.frame(pc = seq(1:94), var.explained  = data.pca.scale$sdev^2/tot.var ) 
         ggplot(var.explained, aes(pc, var.explained)) + geom_bar(stat = "identity") + ggtitle("Bankruptcy")
+        # shows how many variances explained by each PCs. From the first plot we can see around 40 PCs explained more than 90% of variances. 
     })
 
     output$plot3 <- renderPlot({
         #### Pca visualization
-
         data.pca.scale <- prcomp(data[,2:95], center=T ,scale = TRUE)
         data.df <- data.frame(PC1 = data.pca.scale$x[,1], PC2 = data.pca.scale$x[,2], PC3 = data.pca.scale$x[,3], labels = as.factor(data$Bankrupt.))
         ggplot(data.df, aes(PC1, PC2, col = labels)) + geom_point()
+    })
+
+    output$survive_bankrupt <- renderPlot({
+        ggplot(data, aes(Bankrupt.,)) + geom_bar(fill=c("Green","Red")) + theme_bw()+ggtitle("Survive(0) VS Bankruptcy(1)")+theme(plot.title = element_text(hjust = 0.5))+xlab("Survive(0)                              Bankruptcy(1)")
+        cor.matrix<-as.matrix(cor(data))
+        correlation<-sort(cor.matrix[2:95],decreasing = T)
+        #### number of classes,220 bankruptcy vs 6599 survive companies
+
+        print(c((paste0("Bankrupt:",nrow(data[data$Bankrupt.=="1",]))),(paste0("Surivive:",nrow(data[data$Bankrupt.=="0",])))))
+        print("number of classes,220 bankruptcy vs 6599 survive companies")
     })
 }
 
